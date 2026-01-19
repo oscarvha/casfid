ebbfba891aaa9f300bd825790bb6efe83b1b0856
oscar <oscarsoftwaredeveloper@gmail.com>
2026-01-19 16:22:36 +0100

    Improve environment loading in bootstrap by checking for .env file existence

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,13 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+$envFile = dirname(__DIR__).'/.env';
+
+if (method_exists(Dotenv::class, 'bootEnv') && file_exists($envFile)) {
+    (new Dotenv())->bootEnv($envFile);
 }
 
-if ($_SERVER['APP_DEBUG']) {
+if ($_SERVER['APP_DEBUG'] ?? false) {
     umask(0000);
 }
+
