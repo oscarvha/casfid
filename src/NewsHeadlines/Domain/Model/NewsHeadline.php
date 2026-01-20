8b03ba5e920724c277d1cab9aa7d173ba1d78b07
oscar <oscarsoftwaredeveloper@gmail.com>
2026-01-20 10:02:57 +0100

    Add NewsHeadlineCollection and related classes for managing news headlines

--- src/NewsHeadlines/Domain/Model/NewsHeadline.php
+++ src/NewsHeadlines/Domain/Model/NewsHeadline.php
@@ -8,17 +8,85 @@ use App\NewsHeadlines\Domain\ValueObject\NewsHeadlineTitle;
 use App\NewsHeadlines\Domain\ValueObject\NewsHeadlineUrl;
 use DateTimeImmutable;
 
-final class NewsHeadline
+/**
+ *
+ */
+final readonly class NewsHeadline
 {
+    /**
+     * @param NewsHeadlineId $id
+     * @param NewsHeadlineSource $source
+     * @param NewsHeadlineTitle $title
+     * @param NewsHeadlineUrl $url
+     * @param int $position
+     * @param DateTimeImmutable $scrapedAt
+     */
     private function __construct(
-        private NewsHeadlineId     $id,
-        private NewsHeadlineSource $source,
-        private NewsHeadlineTitle  $title,
-        private NewsHeadlineUrl    $url,
-        private int                $position,
-        private DateTimeImmutable  $scrapedAt
+        private NewsHeadlineId             $id,
+        private NewsHeadlineSource         $source,
+        private NewsHeadlineTitle          $title,
+        private NewsHeadlineUrl            $url,
+        private int                        $position,
+        private DateTimeImmutable          $scrapedAt
     ) {}
 
+    /**
+     * @return NewsHeadlineSource
+     */
+    public function source(): NewsHeadlineSource
+    {
+        return $this->source;
+    }
+
+    /**
+     * @return NewsHeadlineTitle
+     */
+    public function title(): NewsHeadlineTitle
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return NewsHeadlineUrl
+     */
+    public function url(): NewsHeadlineUrl
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return int
+     */
+    public function position(): int
+    {
+        return $this->position;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function scrapedAt(): DateTimeImmutable
+    {
+        return $this->scrapedAt;
+    }
+
+    /**
+     * @return NewsHeadlineId
+     */
+    public function id(): NewsHeadlineId
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param NewsHeadlineId $id
+     * @param NewsHeadlineSource $source
+     * @param NewsHeadlineTitle $title
+     * @param NewsHeadlineUrl $url
+     * @param int $position
+     * @param DateTimeImmutable $scrapedAt
+     * @return self
+     */
     public static function create(
         NewsHeadlineId     $id,
         NewsHeadlineSource $source,
