98d9381b5a37e825aa93849a53bb97868610e4eb
oscar <oscarsoftwaredeveloper@gmail.com>
2026-01-20 17:07:20 +0100

    Create migration for news_headlines table with necessary fields and constraints

---
+++ migrations/Version20260120141910.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260120141910 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE news_headlines (source VARCHAR(50) NOT NULL, title VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, position INT NOT NULL, scraped_at DATETIME NOT NULL, id VARCHAR(36) NOT NULL, UNIQUE INDEX UNIQ_B21BB79AF47645AE (url), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE news_headlines');
+    }
+}
